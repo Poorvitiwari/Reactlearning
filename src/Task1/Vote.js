ce73fb6fc071f0a6416f0df8ade4748b7c32d9fd
Poorvi Tiwari <poorvi.tiwari@office365.3pillarglobal.com>
2022-10-13 22:45:21 +0530

    LifeCycle Methods

--- src/Task1/Vote.js
+++ src/Task1/Vote.js
@@ -3,8 +3,7 @@ import './Vote.css'
 class Vote extends Component {
     constructor(props) {
       super(props)
-    
-      this.state =JSON.parse(window.localStorage.getItem('state')) || {
+      this.state ={
          languages:[
             {name:"Javascript",vote:0},
             {name:"Python",vote:0},
@@ -12,12 +11,47 @@ class Vote extends Component {
             {name:"C++",vote:0},
             {name:"Kotlin",vote:0},
          ]
+ 
       }
     }
-    setState(state) {
-        window.localStorage.setItem('state', JSON.stringify(state));
-        super.setState(state);
+    // componentDidMount(){
+    //   // const state=window.localStorage.getItem('state')
+    //   // const items=JSON.parse(state)
+    //   // console.log(items);
+    //   // this.setState(()=>(items))
+    //   const state =JSON.parse(window.localStorage.getItem('state'))
+    //   this.setState(()=>(state))
+    //   console.log(1,this.state,window.localStorage.getItem('state'));
+    //   console.log(state);
+    //   // window.localStorage.setItem('state',JSON.stringify(state))
+
+    // }
+    componentDidUpdate(prevProps,prevState){
+      if(this.state!=prevState){
+        const state =JSON.parse(window.localStorage.getItem('state'))
+      this.setState(()=>(state))
+       console.log(2,this.state,window.localStorage.getItem('state'),prevState);
       }
+      // console.log(prevState.languages);
+      // let data=JSON.parse(window.localStorage.getItem('state'));
+      // console.log(data[0].vote);
+      
+
+        //window.localStorage.setItem('state',JSON.stringify(prevState.languages))
+      
+    }
+    shouldComponentUpdate(prevProps,prevState){
+      const Vote=this.state.languages.map(({vote})=>vote)
+     for(let i=0;i<Vote.length;i++)
+     {
+      if(Vote[i]===4)
+      {
+        return false
+      }
+     }
+     return true
+     
+    }
     reset(state){
        window.localStorage.clear(state);
        this.setState({
@@ -29,34 +63,27 @@ class Vote extends Component {
             {name:"Kotlin",vote:0},
          ]
        })
-
-       
-       console.log("hello");
-       console.log(window.localStorage.getItem('state'));
       }     
-    vote(i){
+      voteIncrement(index){
         let newLanguageVote=this.state.languages;
-        console.log(newLanguageVote[i].name);
-        newLanguageVote[i].vote++;
-
-        console.log("click:",i);
-        console.log(newLanguageVote[i].vote);
+        newLanguageVote[index].vote++;
         this.setState({
-            languages:newLanguageVote
+            languages:newLanguageVote.sort((a,b)=>b.vote-a.vote)
         });
     }
     render() {
+      // console.log(this.state.languages.map(({vote})=>vote));
         return (
         <div>
             <h1>Vote your languages</h1>
             <button onClick={this.reset.bind(this)}>Clear All</button>
             <div className="languages">
             {
-                this.state.languages.map((item,i)=>
-                <div key={i} className='item'>
-                    <div className='voteCount'>{item.vote}</div>
+                this.state.languages.map((item,index)=>
+                <div key={index} className='item'>
+                    <div className='voteCount' >{item.vote}</div>
                     <div className='lnaguageName'>{item.name}</div>
-                    <button onClick={this.vote.bind(this,i)}>Vote ME</button>
+                    <button onClick={this.voteIncrement.bind(this,index)}>Vote ME</button>
                 </div>
                 )
             }
